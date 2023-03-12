58aa67c0261dd3a5bdf00479b1ded593160bdf0f
albberrrt <albberrrt.yayay@gmail.com>
2023-03-11 21:15:47 -0300

    Hello Worlds 01 and 02

---
+++ programming languages/C/HelloWorld_02.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h> //To use "void" functions
+
+void main(){
+    char myString[] = "World";
+    printf("Hello %s!");
+
+    //output "Hello World!";
+}
